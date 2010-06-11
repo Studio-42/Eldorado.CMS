c374f656bb89be2f41d976ae5de6f65896453ebd
Troex Nevelin <troex@fury.scancode.ru>
2010-06-11 23:47:55 +0400

    SpecialOffer plugin update

--- core/plugins/SpecialOffer/elPluginSpecialOffer.class.php
+++ core/plugins/SpecialOffer/elPluginSpecialOffer.class.php
@@ -3,10 +3,10 @@
 class elPluginSpecialOffer extends elPlugin
 {
 	var $_posNfo = array(
-		EL_POS_LEFT   => array('PLUGIN_SPECIAL_OFFER_LEFT',   'default.html'),
-		EL_POS_RIGHT  => array('PLUGIN_SPECIAL_OFFER_RIGHT',  'default.html'),
-		EL_POS_TOP    => array('PLUGIN_SPECIAL_OFFER_TOP',    'default.html'),
-		EL_POS_BOTTOM => array('PLUGIN_SPECIAL_OFFER_BOTTOM', 'default.html')
+		EL_POS_LEFT   => array('PLUGIN_SPECIAL_OFFER_LEFT',   'left-right.html'),
+		EL_POS_RIGHT  => array('PLUGIN_SPECIAL_OFFER_RIGHT',  'left-right.html'),
+		EL_POS_TOP    => array('PLUGIN_SPECIAL_OFFER_TOP',    'top-bottom.html'),
+		EL_POS_BOTTOM => array('PLUGIN_SPECIAL_OFFER_BOTTOM', 'top-bottom.html')
 	);
 
 	function onUnload()
@@ -28,19 +28,31 @@ class elPluginSpecialOffer extends elPlugin
 		{
 			return;
 		}
-		
+
 		$rnd = & elSingleton::getObj('elTE');
 		foreach ($srcs as $src)
 		{
 			// check currect page
 			$pages = $this->_param($src, 'pages', array());
-			if ( !in_array('1', $pages) && !in_array($this->pageID, $pages))
+			if (!in_array('1', $pages) && !in_array($this->pageID, $pages))
 			{
 				continue;
 			}
 
 			// check renderer
-			list($pos, $tplVar, $tpl) = $this->_getPosInfo($this->_param($src, 'pos', EL_POS_LEFT));
+			$mypos = $this->_param($src, 'pos', EL_POS_LEFT);
+			if ($this->pageID == $src) // HACK to change position in IShop item view
+			{
+				if (strstr($_SERVER['REQUEST_URI'], '/item/')) // not 100% true
+				{
+					$mypos = $this->_param($src, 'pos2', 0);
+					if ($mypos === '0')
+					{
+						continue; // next if set to none
+					}
+				}
+			}
+			list($pos, $tplVar, $tpl) = $this->_getPosInfo($mypos);
 			if (!$pos)
 			{
 				continue;
@@ -56,7 +68,7 @@ class elPluginSpecialOffer extends elPlugin
 			// load currency settings
 			$conf = & elSingleton::getObj('elXmlConf');
 			$ishop_conf = $conf->getGroup($src);
-			
+
 			$currency = &elSingleton::getObj('elCurrency');
 			$currency_opts = array(
 				'precision'   => $ishop_conf['pricePrec'],
@@ -81,9 +93,9 @@ class elPluginSpecialOffer extends elPlugin
 				$props = '';
 				foreach ($i->getAnnouncedProperties() as $p)
 				{
-					$props .= '<br/>'.$p['name'].': '.$p['value'];
+					$props .= '<br/>'.$p['name'].': '.$p['value']; // TODO move this to renderer
 				}
-				
+
 				$item = array(
 					'name'  => $i->name,
 					'code'  => $i->code,
@@ -93,23 +105,19 @@ class elPluginSpecialOffer extends elPlugin
 					'img'   => $i->getDefaultTmb(),
 					'props' => $props
 				);
-				
+
 				$b = 'PL_SO.PL_SO_ITEM';
 				$rnd->assignBlockVars($b, $item, 1);
-				
+
 				if (!empty($item['img']))
 				{
 					$b = 'PL_SO.PL_SO_ITEM.PL_SO_ITEM_IMG';
 					$rnd->assignBlockVars($b, $item, 2);
 				}
-
 			}
 			$rnd->parse($tplVar, $tplVar, true, false, true);
 			$GLOBALS['parseColumns'][$pos] = true;
 		}
-		
-		
-		
 	}
 
 	/**
@@ -141,8 +149,8 @@ class elPluginSpecialOffer extends elPlugin
 					$params[$src]['title'] = $data['title_'.$src];
 					$params[$src]['num']   = $data['num_'.$src];
 					$params[$src]['sort']  = $data['sort_'.$src];
-//					$params[$src]['name']  = $data['name_'.$src];
 					$params[$src]['pos']   = $data['pos_'.$src];
+					$params[$src]['pos2']  = $data['pos2_'.$src];
 					$params[$src]['pages'] = $data['pages_'.$src];
 				}
 			}
@@ -183,11 +191,12 @@ class elPluginSpecialOffer extends elPlugin
 			{
 				$box->setAttr('checked', 'on');
 			}
-			$box->add(new elText( 'title_'.$src, m('Title'),                   $this->_param($src, 'title', $pageName)));
-			$box->add(new elSelect( 'num_'.$src, m('How many offers to show'), $this->_param($src, 'num',   1), $nums));
-			$box->add(new elSelect('sort_'.$src, m('How to sort offers'),      $this->_param($src, 'sort',  0), $sort));
-			$box->add(new elSelect( 'pos_'.$src, m('Position on page'),        $this->_param($src, 'pos',   EL_POS_TOP), $GLOBALS['posLRTB']));
-			$ms = & new elMultiSelectList('pages_'.$src, m('Site pages'),      $this->_param($src, 'pages', array(1)), $pages);
+			$box->add(new elText( 'title_'.$src, m('Title'),                       $this->_param($src, 'title', $pageName)));
+			$box->add(new elSelect( 'num_'.$src, m('How many offers to show'),     $this->_param($src, 'num',   1), $nums));
+			$box->add(new elSelect('sort_'.$src, m('How to sort offers'),          $this->_param($src, 'sort',  0), $sort));
+			$box->add(new elSelect( 'pos_'.$src, m('Position on page'),            $this->_param($src, 'pos',   EL_POS_TOP), $GLOBALS['posLRTB']));
+			$box->add(new elSelect('pos2_'.$src, m('Position on IShop item page'), $this->_param($src, 'pos2',  EL_POS_TOP), $GLOBALS['posNLRTB']));
+			$ms = & new elMultiSelectList('pages_'.$src, m('Site pages'),          $this->_param($src, 'pages', array(1)), $pages);
 			$ms->setSwitchValue(1);
 			$box->add($ms);
 			$this->form->add($box);

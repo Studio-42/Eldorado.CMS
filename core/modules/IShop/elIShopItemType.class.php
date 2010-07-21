bb0dc3f6b8a7a7aed67310cd098b8e9d1b7d4f4f
Dmitry Levashov <dio@std42.ru>
2010-07-21 21:22:04 +0400

    remove old methods from iShopType

--- core/modules/IShop/elIShopItemType.class.php
+++ core/modules/IShop/elIShopItemType.class.php
@@ -1,18 +1,20 @@
 <?php
-
-class elIShopItemType extends elDataMapping
-{
-	var $tb         = '';
-	var $tbp        = '';
-	var $ID         = 0;
-	var $name       = '';
-	var $descrip    = '';
-	var $crtime     = 0;
-	var $mtime      = 0;
-	var $_props     = null;
-	var $_prop      = null;
-	var $_objName   = 'Product type';
-	var $_factory   = null;
+/**
+ * Product type
+ *
+ * @package iShop
+ **/
+class elIShopItemType extends elDataMapping {
+	var $tb       = '';
+	var $tbp      = '';
+	var $ID       = 0;
+	var $name     = '';
+	var $descrip  = '';
+	var $crtime   = 0;
+	var $mtime    = 0;
+	var $_props   = null;
+	var $_objName = 'Product type';
+	var $_factory = null;
 
 	/**
 	 * add count items to default array
@@ -34,7 +36,6 @@ class elIShopItemType extends elDataMapping
 		return $this->_factory->ic->count(EL_IS_ITYPE, $this->ID);
 	}
 	
-
 	/**
 	 * Return all properties
 	 *
@@ -60,6 +61,18 @@ class elIShopItemType extends elDataMapping
 	}
 
 	/**
+	 * Return product type with properties
+	 *
+	 * @return void
+	 **/
+	function delete($ref = null) {
+		foreach ($this->props as $p) {
+			$p->delete();
+		}
+		parent::delete();
+	}
+
+	/**
 	 * Load properties if not loaded and return
 	 *
 	 * @return array
@@ -78,78 +91,13 @@ class elIShopItemType extends elDataMapping
 	}
 
 	/**
-	 * Return product type with properties
-	 *
-	 * @return void
-	 **/
-	function delete($ref = null) {
-		foreach ($this->props as $p) {
-			$p->delete();
-		}
-		parent::delete();
-	}
-
-  function getProperyFormElement($pID)
-  {
-    return !empty($this->props[$pD]) ? $this->props[$pD]->getFormElement() : null;
-  }
-
-  function editProperty($pID)
-  {
-    if ( !empty($this->props[$pID]) )
-    {
-      $this->_prop = $this->props[$pID];
-    }
-    else
-    {
-      $this->_prop = $this->_factory->getProperty($pID);
-      $this->_prop->attr('t_id', $this->ID);
-    }
-    return $this->_prop->editAndSave( array('itName'=>$this->name, 'maxSortNdx'=>sizeof($this->props)) );
-  }
-
-  function removeProperty($pID)
-  {
-    if ( !$this->isPropertyExists($pID) )
-    {
-      return elThrow( E_USER_WARNING, 'There is no object "%s" with ID="%d"', array(m('Property'), $pID) );
-    }
-    $this->props[$pID]->delete();
-    elMsgBox::put( sprintf(m('Object "%s" "%s" was deleted'), m('Property'), $this->props[$pID]->name) );
-    unset($this->props[$pID]);
-    return true;
-  }
-
-  function editPropertyDependance($pID)
-  {
-    if ( !$this->isPropertyExists($pID) )
-    {
-      return elThrow( E_USER_WARNING, 'There is no object "%s" with ID="%d"', array(m('Property'), $pID) );
-    }
-    $this->_prop = $this->props[$pID];
-    if ( !$this->_prop->isDependAvailable()
-    && !empty($this->_prop->dependID)
-    && !empty($this->props[$this->_prop->dependID]) )
-    {
-      return elThrow( E_USER_WARNING, 'Could not edit property dependance' );
-    }
-    return $this->_prop->editDependance( $this->name, $this->props[$this->_prop->dependID] );
-
-  }
-
-  function formToHtml()
-  {
-    return $this->_form ? $this->_form->toHtml() : ( $this->_prop ? $this->_prop->formToHtml() : '' );
-  }
-
-	/**
 	 * create form
 	 *
 	 * @return void
 	 **/
 	function _makeForm($params = null) {
 		parent::_makeForm();
-		$this->_form->add( new elText('name', m('Type name'), $this->name));
+		$this->_form->add(new elText('name', m('Type name'), $this->name));
 		$this->_form->setRequired('name');
 	}
 
@@ -174,13 +122,13 @@ class elIShopItemType extends elDataMapping
 	 **/
 	function _initMapping() {
 		return array(
-			'id'     => 'ID', 
-			'name'   => 'name', 
+			'id'      => 'ID', 
+			'name'    => 'name', 
 			'descrip' => 'descrip',
-			'crtime' => 'crtime',  
-			'mtime'  => 'mtime');
+			'crtime'  => 'crtime',  
+			'mtime'   => 'mtime');
 	}
 
-}
+} // END class 
 
 ?>
